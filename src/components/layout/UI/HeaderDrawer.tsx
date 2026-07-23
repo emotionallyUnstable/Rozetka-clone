70ba5e751a8acb59012bb1e918d18bcd3f41520e
emotionallyUnstable <95876061+emotionallyUnstable@users.noreply.github.com>
2026-07-23 22:52:59 +0300

    Drawer update

--- src/components/layout/UI/HeaderDrawer.tsx
+++ src/components/layout/UI/HeaderDrawer.tsx
@@ -1,4 +1,5 @@
-
+import { X } from 'lucide-react';
+import logo from "../../../assets/ROZETKA-Logo-L3-B-RGB.png";
 interface HeaderDrawerProps {
     isOpen: boolean;
     onClose: () => void;
@@ -7,7 +8,6 @@ interface HeaderDrawerProps {
 export default function HeaderDrawer({ isOpen, onClose } : HeaderDrawerProps) {
     return(
         <>
-
             <div onClick={onClose}
              className={`fixed inset-0 bg-black/50 z-50 transition-opacity duration-300
                          ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}
@@ -16,15 +16,46 @@ export default function HeaderDrawer({ isOpen, onClose } : HeaderDrawerProps) {
 
             <aside className={`fixed top-0 left-0 h-full w-[320px] bg-white z-50
                               transform transition-transform duration-300 ease-in-out
-                              ${isOpen ? "translate-x-0" : "-translate-x-full"}`} />
-{/* 
-            <div className="flex items-center justify-between p-4 border-b">
-                <span className="font-semibold">Меню</span>
-                <button onClick={onClose} className="p-2 rounded-lg hover:bg-gray-100">
+                              ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+            >
 
-                </button>
-            </div> */}
-                                  
+          <div className="flex items-center justify-between p-2 border-b bg-gray-700">
+            <img className='h-10 w-50'
+             src={logo} alt="#" />
+          <button onClick={onClose} className="p-2 rounded-lg hover:bg-gray-100">
+            <X size={15} />
+          </button>
+        </div>
+
+        <nav className='flex text-black'>
+            <ul>
+                <li>
+                    <button>Довідковий центр</button>
+                </li>
+                <li>
+                    <div>
+                        <h1 className=''
+                        >Увійдіть, щоб отримувати рекомендації, персональні бонуси і знижки.</h1>
+                        <button className=''
+                        >Увійдіть в особистий кабінет</button>
+                    </div>
+                </li>
+
+                <li>
+                    <button>Кошик</button>
+                </li>
+
+                <li>
+                    <button>Списки бажань</button>
+                </li>
+
+                <li>
+                    <button>Списки порівнянь</button>
+                </li>
+            </ul>
+        </nav>
+
+             </aside>     
         </>
     )
 }
